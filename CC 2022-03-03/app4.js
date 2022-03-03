0e99082582f0cf6a6457ab48b8c269776cc28fc0
moomoonnow <moomoonnow@gmail.com>
2022-03-03 18:13:26 -0500

    add 03-03

---
+++ CC 2022-03-03/app4.js
@@ -0,0 +1,18 @@
+// 8 kyu
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+// code given: 
+// function multiply(a, b){
+//     a * b
+// }
+  
+// ----------------------
+// parameters: given non-working code
+// return: working codde
+// pseudocode: a * b works. but without a return, it will not be returned back to the user
+// important take-away. don't forget RETURN
+function multiply(a, b){
+    let result = a * b;
+    return result;
+}
