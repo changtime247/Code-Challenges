5d42f5aed1eef43dc9fbe844e980bd8ca17cc1f1
moomoonnow <moomoonnow@gmail.com>
2022-03-05 17:45:01 -0500

    add 03-05 wrapping values inside an object

---
+++ CC 2022-03-05/app.js
@@ -0,0 +1,23 @@
+// 8 kyu
+// Semi-Optional
+// We have implemented a function wrap(value) that takes a value of arbitrary type and wraps it in a new JavaScript Object or Python Dict setting the 'value' key on the new Object or Dict to the passed-in value.
+// So, for example, if we execute the following code:
+// wrapper_obj = wrap("my_wrapped_string"); 
+//  # wrapper_obj should be  {"value":"my_wrapped_string"}
+// We would then expect the following statement to be true:
+// wrapper_obj["value"] == "my_wrapped_string"
+// Unfortunately, the code is not working as designed. Please fix the code so that it behaves as specified.
+
+// ----------------------
+// parameters: some arbitrary value
+// return: value as a property of an object
+// example:
+// ({343}).value --> 343
+// pseudocode: set value as a property of an object. 
+
+function wrap(value) {
+    let obj = {
+        value
+    }
+    return obj
+}
