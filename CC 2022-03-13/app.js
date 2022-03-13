5c09de9560dde25c03c46675c52bdc391f5238a9
moomoonnow <moomoonnow@gmail.com>
2022-03-13 12:53:59 -0400

    add 03-13 using map

---
+++ CC 2022-03-13/app.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+// using map workings best to iterate through each element and return a modified array with the same amount of elements as it started with
+
+function maps(x){
+    return x.map(ele=>ele*2)
+}
