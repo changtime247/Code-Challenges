3cd025254228b52424cd2e439116bb717e8786b9
moomoonnow <moomoonnow@gmail.com>
2022-03-18 09:33:19 -0400

    add 03-18

---
+++ CC 2022-03-18/app.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Function 3 - multiplying two numbers
+// Implement a function which multiplies two numbers.
+
+// ----------------------
+// parameters: two numbers given
+// return product of the numbers
+// Examples
+// 2*3 --> 6
+// pseudocode: multiply the two variables
+
+const multiply = (a,b) => a*b
