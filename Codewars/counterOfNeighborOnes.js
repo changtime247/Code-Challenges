97e53ac6255873eb6caadb0cd3155ec36fd4cebe
andrew <changtime247@gmail.com>
2022-10-07 08:51:32 -0400

    consolidated folders

---
+++ Codewars/counterOfNeighborOnes.js
@@ -0,0 +1,19 @@
+// 7 kyu
+// Counter of neighbor ones
+// Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+// Example
+// [1, 1, 1, 0, 1] -> [3,1]
+// [1, 1, 1, 1, 1] -> [5]
+// [0, 0, 0, 0, 0] -> []
+
+// ----------------------
+// Parameter: Given an array of 1s and 0s.
+// Return the number of times that the string path passes the valley.
+// Examples:
+// countingValleys('FUFFDDFDUDFUFUF'), 1
+// Pseudocode:
+// At the start of string path, the level is at 0.
+// Iterate through the string, incrementing the level by one on 'U' and decrementing by one on 'D'.
+// Return the number of times that the path passes through a valley
+// ('passing through a valley' means anytim
+
